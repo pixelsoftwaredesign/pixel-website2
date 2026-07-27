c72b74ce0baa4c9d96b2d3b60939e3e6d57457a0
PixelSoft <pixelsoftwaredesign@gmail.com>
2026-07-27 23:26:12 +0100

    fix: create hajerghiloufi user with 500 TND

---
+++ pixel_project/studio/migrations/0023_create_user_hajerghiloufi.py
@@ -0,0 +1,36 @@
+from django.db import migrations
+from django.contrib.auth.hashers import make_password
+
+
+def create_user_hajerghiloufi(apps, schema_editor):
+    User = apps.get_model('auth', 'User')
+    Wallet = apps.get_model('studio', 'Wallet')
+
+    user, created = User.objects.get_or_create(
+        username='hajerghiloufi',
+        defaults={
+            'email': 'hajer@pixelsoftwaredesign.xyz',
+            'is_staff': False,
+            'is_superuser': False,
+            'password': make_password('pixelsoftwaredesign'),
+        },
+    )
+    if not created:
+        user.password = make_password('pixelsoftwaredesign')
+        user.save()
+
+    wallet, _ = Wallet.objects.get_or_create(user=user)
+    if wallet.solde == 0:
+        wallet.solde = 500.00
+        wallet.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('studio', '0022_reset_pixelsoftwaredesign_password'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_user_hajerghiloufi, migrations.RunPython.noop),
+    ]
